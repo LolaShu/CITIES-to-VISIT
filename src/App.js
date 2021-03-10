ec680c59bb3133be62233e2e07eabf1dacd049b8
olgashnurenko <shnurenkolga@gmail.com>
2021-03-09 22:56:21 -0500

    added styling and city list element

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.scss'
-// import firebase from './firebase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck} from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import DisplayPhotos from './DisplayPhoto.js';
 import Form from './Form.js'
+import firebase from './firebase.js';
 
 // App.js
 function App() {
 
   const [randomPhotos, setRandomPhotos] = useState([]);
-  const [userChoice, setUserChoice] = useState('')
-  // const [filteredPhotos, setFliteredPhotos] = useState([]);
-  // const value = 'amsterdam';
+  const [userChoice, setUserChoice] = useState('skyline')
+  const [citiesArray, setCitiesArray] = useState([]);  
+  const [textInput, setTextInput] = useState("");
   useEffect(() => {
 
     axios({
@@ -41,12 +43,44 @@ function App() {
 
   }, [userChoice]);
 
-  // //defining query parameter
+
   const getPhotos = (choice) => {
 
 
     setUserChoice(choice)
 
+  }
+
+  useEffect(() => {
+    const dbRef = firebase.database().ref();
+    dbRef.on('value', (data) => {
+      console.log(data.val());
+      const cityData = data.val();
+      const cityBag = [];
+      for (let cityKey in cityData) {
+        cityBag.push({
+          uniqueKey: cityKey,
+          title: cityData[cityKey]
+        });
+      }
+      setCitiesArray(cityBag);
+    });
+
+  }, []);
+
+  const handleChange = (event) => {
+    setTextInput(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const dbRef = firebase.database().ref();
+    dbRef.push(textInput);
+    setTextInput("");
+  }
+  const handleClick = (cityUniqueId) => {
+    const dbRef = firebase.database().ref();
+    dbRef.child(cityUniqueId).remove();
   }
 
 
@@ -61,11 +95,34 @@ function App() {
           </header>
           <Form getPhotos={getPhotos} />
           <DisplayPhotos photos={randomPhotos} />
+
+          <div className="cities-box">
+          <form action="" onSubmit={handleSubmit} className="more-cities">
+          <label htmlFor="citiesToVisit" className="to-visit"><h2>The next city I visit will be</h2></label>
+          <div className="cities-to-visit">
+          <input type="text" id="cityToVisit" required onChange={handleChange} value={textInput} />
+          <button>add</button>
+          </div>
+        </form>     
+        
+        <ul className="cities-list">
+          {
+            citiesArray.map((city) => {
+              return (
+                <li key={city.uniqueKey}>                  
+                  <button onClick={() => { handleClick(city.uniqueKey) }}><FontAwesomeIcon icon={faCheck} /></button>
+                  <span>{city.title}</span>
+                </li>
+              )
+            })
+          }
+        </ul>
+          </div>
+        </div>
         </div>
         <div className="background-img">
-          
         </div>
-      </div>
+      
 
       <footer>
         <p>Created by Olga Sh at <a href="https://junocollege.com/">Juno college</a> </p>
--- src/DisplayPhoto.js
+++ src/DisplayPhoto.js
@@ -1,6 +1,5 @@
 //DisplayPhotos.js
-// import Form from './Form.js'
-// import 'react-slideshow-image/dist/styles.css';
+
 import { Slide } from 'react-slideshow-image';
 
 const DisplayPhotos = ({photos}) => {
@@ -9,46 +8,27 @@ const DisplayPhotos = ({photos}) => {
         duration: 5000,
         canSwipe: true,
     };
-    return (    
-        
-        <div className="slide">
-        
+    return (       
+        <div>
             <Slide {...fadeProperties}>
                 {photos.map((photo) => {
                     
                     return (
+                        
                         <div className="slideContainer" key={photo.id}>
                             <div className="each-fade" >
                                 <div>
                                     <img src={photo.urls.regular} alt={photo.alt_description} />
                                 </div>
-                                <h2>{photo.user.name}</h2>
+                                <h2>{photo.user.first_name}</h2>
                             </div>
                         
                         </div>
                     )
                 })}
             </Slide>
+        
         </div>
-    
     )
 }
 export default DisplayPhotos
-
-    // <>
-    // {/* <h2>Photos!</h2> */ }
-
-    // < div className = "photos" >
-    // {
-    //     photos.map((photo) => {
-    //         return (
-    //             <div className="photo-container" key={photo.id}>
-    //                 <img src={photo.urls.regular} alt={photo.alt_description} />
-    //                 <h2>{photo.user.first_name}</h2>
-    //             </div>
-
-    //         );
-    //     })
-    // }
-    //                 </div >
-    //             </>
